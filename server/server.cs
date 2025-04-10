5b0fa9ce23de916f2e977f53bc5f35577e2bba37
Eduardo <Eduardo@DESKTOP-5OH9N8H>
2025-04-10 10:51:20 +0300

    Добавьте файлы проекта.

---
+++ server/server.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    internal class server
+    {
+        static void Main(string[] args)
+        {
+            // Устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+            // Создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            // Назначаем сокет локальной конечной точке  ислушаем входящие сокеты
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+                // Начинаем слушать соединения
+                while (true)
+                {
+                    Console.WriteLine("Ожидаем соединение через порт {0}", ipEndPoint); // Программа приостанавливается, ожидая входящее соединение
+                    Socket handler = sListener.Accept();
+                    string data = null;
+                    // Мы дождались клиента, пытающегося с нами соединиться
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    // Показываем данные на консоли
+                    Console.Write("Полученный текст: " + data + "\n\n");
+                    Replacer(ref data); // изменяем сообщение
+                    string reply = $"Спасибо за запрос в {data.Length} символов\n\n{data}\n\nВ сообщениях заменены [] на ()"; // формируем ответ-сообщение
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);// отправляем ответ клиенту
+                    if (data.IndexOf("<TheEnd>") > -1)
+                    {
+                        Console.WriteLine("Сервер завершил соединение с клиентом.");
+                        break;
+                    }
+                    //отключаем передачу и прием; закрываем соединение
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        //Добавленный метод по условию задачи
+        static void Replacer(ref string input)
+        {
+            input = input.Replace('[', '(').Replace(']', ')');
+        }
+    }
+}
